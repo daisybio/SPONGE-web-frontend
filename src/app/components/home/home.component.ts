6a5da9eefc449e0217c82ee1e9844bae0fb46955
ReisysFelicity <veronika.stiegler@t-online.de>
2019-08-31 15:35:16 +0200

    resolved cofilcts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
+import * as sigma from 'sigma-webpack';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  home_search: string;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams
+    .subscribe(params => {
+      this.home_search = params.home_search;
+    });
+
+    if (this.home_search != undefined) {
+      // check if gene or mirna
+      if (this.home_search ) {
+        // gene
+      }
+    }
+    
+    $('#home_search_button').click( () => {
+      let input = $('#home_search').val()
+      
+
+
+    })
+
+    
   }
 
+  
+  
 }
+
+
