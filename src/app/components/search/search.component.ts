933376632b4b3e706eb8e7882df66723618b14f2
Maiykol <michael.hartung96@googlemail.com>
2019-09-04 22:39:01 +0300

    search filter tables

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -25,6 +25,45 @@ export class SearchComponent implements OnInit {
       search_key = params.search_key;
     });
 
+    function push_interaction_filters(table) {
+      $.fn.dataTable.ext.search.push(
+        // filter for mscor
+        function( settings, data, dataIndex) {
+          if ( settings.nTable.id !== table ) {
+            return true;
+          }
+          var mscore_min = parseFloat( $('#mscore_min_'+table).val());
+          var mscore_max = parseFloat( $('#mscore_max_'+table).val());
+          var mscore = parseFloat( data[5] ) || 0; // use data for the mscor column
+          if (( isNaN( mscore_min ) && isNaN( mscore_max ) ) ||
+                ( isNaN( mscore_min ) && mscore <= mscore_max ) ||
+                ( mscore_min <= mscore && isNaN( mscore_max ) ) ||
+                ( mscore_min <= mscore && mscore <= mscore_max ))
+          {
+              return true;
+          }
+          return false;  
+        },
+        //  filter for pvalue
+        function( settings, data, dataIndex) {
+          if ( settings.nTable.id !== table ) {
+            return true;
+          }
+          var pvalue_min = parseFloat( $('#pvalue_min_'+table).val());
+          var pvalue_max = parseFloat( $('#pvalue_max_'+table).val());
+          var pvalue = parseFloat( data[6] ) || 0; // use data for the pvalue column
+          if (( isNaN( pvalue_min ) && isNaN( pvalue_max ) ) ||
+            ( isNaN( pvalue_min ) && pvalue <= pvalue_max ) ||
+            ( pvalue_min <= pvalue && isNaN( pvalue_max ) ) ||
+            ( pvalue_min <= pvalue && pvalue <= pvalue_max ) )
+            {
+              return true;
+            }
+          return false;
+          }
+      );
+    }
+
     function parse_cerna_response(response) {
       response.forEach(interaction => {
         let interaction_info = {};
@@ -70,6 +109,7 @@ export class SearchComponent implements OnInit {
       let list_diseases = $('#list_diseases')
       for (let disease in parsed_search_result['diseases']) {
         let disease_trimmed = disease.split(' ').join('')
+        let table_id = disease_trimmed+"-table"
         let accordion_card = "<div class='card'>"+
         "<div class='card-header' id='heading_"+disease_trimmed+"'>"+
         "  <h5 class='mb-0'>"+
@@ -80,20 +120,44 @@ export class SearchComponent implements OnInit {
         "</div>"+
         "<div id='collapse_"+disease_trimmed+"' class='collapse show' aria-labelledby='headingOne' data-parent='#disease_accordion'>"+
           "<div class='card-body'>"+
-           ""
+            "<button class='btn btn-secondary button-margin' type='button' data-toggle='collapse' data-target='#control_"+table_id+"' aria-expanded='false'>"+
+                "Options"+
+            "</button>"+
+            "<div class='collapse' id='control_"+table_id+"'>"+
+                "<div class='card card-body'>"+
+                "<div>"+
+                  "<p>Set filter for mscor</p>"+
+                  "<span>Minimum: </span><input type='text' id='mscore_min_"+table_id+"' name='mscore_min'>&nbsp;"+
+                  "<span>Maximum: </span><input type='text' id='mscore_max_"+table_id+"' name='mscore_max'>"+
+                "</div>"+
+                "<hr>"+
+                "<div>"+
+                    "<p>Set filter for P-value</p>"+
+                    "<span>Minimum: </span><input type='text' id='pvalue_min_"+table_id+"' name='pvalue_min'>&nbsp;"+
+                    "<span>Maximum: </span><input type='text' id='pvalue_max_"+table_id+"' name='pvalue_max'>"+
+                "</div>"+
+              "</div>"+
+            "</div>"+
+            "<div class='card-body-table'></div>"+
+           ""+
           "</div>"+
         "</div>"+
         "</div>"
         $('#disease_accordion').append(accordion_card)
-
-        let table = helper.buildTable(
+        
+        let html_table = helper.buildTable(
           parsed_search_result['diseases'][disease], 
-          disease_trimmed+"-table", 
+          table_id, 
           Object.keys(parsed_search_result['diseases'][disease][0])
           )
-        $('#collapse_'+disease_trimmed).find('.card-body').html(table)
-
-        $("#"+disease_trimmed+"-table").DataTable()
+        $('#collapse_'+disease_trimmed).find('.card-body-table').html(html_table)
+        
+        push_interaction_filters(table_id)
+        var table = $("#"+table_id).DataTable()
+        $('#mscore_min_'+table_id+', #mscore_max_'+table_id+', #pvalue_min_'+table_id+', #pvalue_max_'+table_id).keyup(()=>{
+          table.draw()
+        })
+        
       }
     }
 
