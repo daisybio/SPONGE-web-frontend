13cb228a23d3f160e1bb631850c4883750f3c65a
Maiykol <michael.hartung96@googlemail.com>
2019-09-08 20:02:23 +0300

    all hsa numbers start with hsa-miR-

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -14,7 +14,7 @@ export class SearchComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    
+
     const controller = new Controller()
     const helper = new Helper()
 
@@ -22,9 +22,9 @@ export class SearchComponent implements OnInit {
     var parsed_search_result: any;
 
     this.route.queryParams
-    .subscribe(params => {
-      search_key = params.search_key;
-    });
+      .subscribe(params => {
+        search_key = params.search_key;
+      });
 
 
     main()
@@ -61,7 +61,7 @@ export class SearchComponent implements OnInit {
               $('#loading_spinner_results').addClass('hidden')
             }
           })
-        } else if (search_key.startsWith('hs')){
+        } else if (search_key.startsWith('hsa-miR-')) {
           // key is hsa number
           controller.get_miRNA_interactions_all({
             limit: 11,
@@ -90,7 +90,7 @@ export class SearchComponent implements OnInit {
     function parse_mirna_response(response) {
       // get information aboout search key
       let key_information = response[0]['mirna']
-      let key_information_sentence = "Results for "+key_information['mir_ID']+" ("+key_information['hs_nr']+")"
+      let key_information_sentence = "Results for " + key_information['mir_ID'] + " (" + key_information['hs_nr'] + ")"
       $('#key_information').html(key_information_sentence)
 
       // parse response
@@ -102,7 +102,7 @@ export class SearchComponent implements OnInit {
         row['Gene2'] = gene_gene['gene2']['ensg_number']
         row['Gene2 Symbol'] = gene_gene['gene2']['gene_symbol']
         let disease = gene_gene['run']['dataset']
-        if (disease in parsed_search_result['diseases']){
+        if (disease in parsed_search_result['diseases']) {
           parsed_search_result['diseases'][disease].push(row)
         } else {
           parsed_search_result['diseases'][disease] = [row]
@@ -112,77 +112,75 @@ export class SearchComponent implements OnInit {
       // build html for response_data
       for (let disease in parsed_search_result['diseases']) {
         let disease_trimmed = disease.split(' ').join('')
-        let table_id:string = disease_trimmed+"-table"
-        let accordion_card = "<div class='card'>"+
-        "<div class='card-header' id='heading_"+disease_trimmed+"'>"+
-          "<h5 class='mb-0'>"+
-            "<button class='btn btn-link collapsed' data-toggle='collapse' data-target='#collapse_"+disease_trimmed+"' aria-expanded='false' aria-controls='collapse_"+disease_trimmed+"'>"+
-              disease+
-            "</button>"+
-          "</h5>"+
-        "</div>"+
-        "<div id='collapse_"+disease_trimmed+"' class='collapse' aria-labelledby='headingOne' data-parent='#disease_accordion'>"+
-          "<div class='card-body'>"+ 
-              "<div class='card-body-table'></div>"+
-            "</div>"+
-        "</div>"
+        let table_id: string = disease_trimmed + "-table"
+        let accordion_card = "<div class='card'>" +
+          "<div class='card-header' id='heading_" + disease_trimmed + "'>" +
+          "<h5 class='mb-0'>" +
+          "<button class='btn btn-link collapsed' data-toggle='collapse' data-target='#collapse_" + disease_trimmed + "' aria-expanded='false' aria-controls='collapse_" + disease_trimmed + "'>" +
+          disease +
+          "</button>" +
+          "</h5>" +
+          "</div>" +
+          "<div id='collapse_" + disease_trimmed + "' class='collapse' aria-labelledby='headingOne' data-parent='#disease_accordion'>" +
+          "<div class='card-body'>" +
+          "<div class='card-body-table'></div>" +
+          "</div>" +
+          "</div>"
         $('#disease_accordion').append(accordion_card)
-        
+
         let html_table = helper.buildTable(
-          parsed_search_result['diseases'][disease], 
-          table_id, 
+          parsed_search_result['diseases'][disease],
+          table_id,
           Object.keys(parsed_search_result['diseases'][disease][0])
-          )
-        $('#collapse_'+disease_trimmed).find('.card-body-table').html(html_table)
-        
-        var table = $("#"+table_id).DataTable({
+        )
+        $('#collapse_' + disease_trimmed).find('.card-body-table').html(html_table)
+
+        var table = $("#" + table_id).DataTable({
           orderCellsTop: true,
         })
         helper.colSearch(table_id, table)
-        
+
         // make rows selectable
-        $('#'+table_id+' tbody').on( 'click', 'tr', function () {
+        $('#' + table_id + ' tbody').on('click', 'tr', function () {
           $(this).toggleClass('selected');
         })
       }
     }
 
-    function push_interaction_filters(table_id:string) {
+    function push_interaction_filters(table_id: string) {
       $.fn.dataTable.ext.search.push(
         // filter for mscor
-        function( settings, data, dataIndex) {
-          if ( settings.nTable.id !== table_id ) {
+        function (settings, data, dataIndex) {
+          if (settings.nTable.id !== table_id) {
             return true;
           }
-          var mscore_min = parseFloat( $('#mscore_min_'+table_id).val().toString());
-          var mscore_max = parseFloat( $('#mscore_max_'+table_id).val().toString());
-          var mscore = parseFloat( data[5] ) || 0; // use data for the mscor column
-          if (( isNaN( mscore_min ) && isNaN( mscore_max ) ) ||
-                ( isNaN( mscore_min ) && mscore <= mscore_max ) ||
-                ( mscore_min <= mscore && isNaN( mscore_max ) ) ||
-                ( mscore_min <= mscore && mscore <= mscore_max ))
-          {
-              return true;
+          var mscore_min = parseFloat($('#mscore_min_' + table_id).val().toString());
+          var mscore_max = parseFloat($('#mscore_max_' + table_id).val().toString());
+          var mscore = parseFloat(data[5]) || 0; // use data for the mscor column
+          if ((isNaN(mscore_min) && isNaN(mscore_max)) ||
+            (isNaN(mscore_min) && mscore <= mscore_max) ||
+            (mscore_min <= mscore && isNaN(mscore_max)) ||
+            (mscore_min <= mscore && mscore <= mscore_max)) {
+            return true;
           }
-          return false;  
+          return false;
         },
         //  filter for pvalue
-        function( settings, data, dataIndex) {
-          if ( settings.nTable.id !== table_id ) {
+        function (settings, data, dataIndex) {
+          if (settings.nTable.id !== table_id) {
             return true;
           }
-          var pvalue_min = parseFloat( $('#pvalue_min_'+table_id).val().toString());
-          var pvalue_max = parseFloat( $('#pvalue_max_'+table_id).val().toString());
-          var pvalue = parseFloat( data[6] ) || 0; // use data for the pvalue column
-          if (( isNaN( pvalue_min ) && isNaN( pvalue_max ) ) ||
-            ( isNaN( pvalue_min ) && pvalue <= pvalue_max ) ||
-            ( pvalue_min <= pvalue && isNaN( pvalue_max ) ) ||
-            ( pvalue_min <= pvalue && pvalue <= pvalue_max ) )
-            {
-              return true;
-            }
-          return false;
+          var pvalue_min = parseFloat($('#pvalue_min_' + table_id).val().toString());
+          var pvalue_max = parseFloat($('#pvalue_max_' + table_id).val().toString());
+          var pvalue = parseFloat(data[6]) || 0; // use data for the pvalue column
+          if ((isNaN(pvalue_min) && isNaN(pvalue_max)) ||
+            (isNaN(pvalue_min) && pvalue <= pvalue_max) ||
+            (pvalue_min <= pvalue && isNaN(pvalue_max)) ||
+            (pvalue_min <= pvalue && pvalue <= pvalue_max)) {
+            return true;
           }
+          return false;
+        }
       );
     }
 
@@ -222,9 +220,9 @@ export class SearchComponent implements OnInit {
         //interaction_info['gene'] = interaction[gene_to_extract]['gene_ID']
         interaction_info['mscor'] = interaction['mscore']
         interaction_info['p-value'] = interaction['p_value']
-        
+
         parsed_search_result['diseases'][disease].push(interaction_info)
-      
+
       }); // end for each
 
       // Set key-gene information
@@ -233,11 +231,11 @@ export class SearchComponent implements OnInit {
         gene_symbol: parsed_search_result['key']['gene_symbol'],
         chromosome: parsed_search_result['key']['chromosome_name']
       }
-      let key_information_sentence = "For gene "+key_information['gene']
+      let key_information_sentence = "For gene " + key_information['gene']
       if (key_information['gene_symbol'] != '') {
-        key_information_sentence += " ("+key_information['gene_symbol']+")" 
+        key_information_sentence += " (" + key_information['gene_symbol'] + ")"
       }
-      key_information_sentence += " on chromosome "+key_information['chromosome']
+      key_information_sentence += " on chromosome " + key_information['chromosome']
 
       $('#key_information').html(key_information_sentence)
 
@@ -245,61 +243,61 @@ export class SearchComponent implements OnInit {
       let list_diseases = $('#list_diseases')
       for (let disease in parsed_search_result['diseases']) {
         let disease_trimmed = disease.split(' ').join('')
-        let table_id:string = disease_trimmed+"-table"
-        let accordion_card = "<div class='card'>"+
-        "<div class='card-header' id='heading_"+disease_trimmed+"'>"+
-        "  <h5 class='mb-0'>"+
-            "<button class='btn btn-link collapsed' data-toggle='collapse' data-target='#collapse_"+disease_trimmed+"' aria-expanded='false' aria-controls='collapse_"+disease_trimmed+"'>"+
-              disease+
-            "</button>"+
-          "</h5>"+
-        "</div>"+
-        "<div id='collapse_"+disease_trimmed+"' class='collapse' aria-labelledby='headingOne' data-parent='#disease_accordion'>"+
-          "<div class='card-body'>"+ 
-            "<button class='btn btn-secondary button-margin' type='button' data-toggle='collapse' data-target='#control_"+table_id+"' aria-expanded='false'>"+
-                "Options"+
-            "</button>"+
-            "<button class='btn btn-secondary button-margin' type='button' id='expression_"+disease_trimmed+"'>"+
-              "Raw expression data"+
-            "</button>"+
-            "<div class='collapse' id='control_"+table_id+"'>"+
-                "<div class='card card-body'>"+
-                  "<div>"+
-                    "<p>Set filter for mscor</p>"+
-                    "<span>Minimum: </span><input type='text' id='mscore_min_"+table_id+"' name='mscore_min'>&nbsp;"+
-                    "<span>Maximum: </span><input type='text' id='mscore_max_"+table_id+"' name='mscore_max'>"+
-                  "</div>"+
-                  "<hr>"+
-                  "<div>"+
-                      "<p>Set filter for P-value</p>"+
-                      "<span>Minimum: </span><input type='text' id='pvalue_min_"+table_id+"' name='pvalue_min'>&nbsp;"+
-                      "<span>Maximum: </span><input type='text' id='pvalue_max_"+table_id+"' name='pvalue_max'>"+
-                  "</div>"+
-                "</div>"+
-              "</div>"+
-              "<div class='card-body-table'></div>"+
-            "</div>"+
-        "</div>"
+        let table_id: string = disease_trimmed + "-table"
+        let accordion_card = "<div class='card'>" +
+          "<div class='card-header' id='heading_" + disease_trimmed + "'>" +
+          "  <h5 class='mb-0'>" +
+          "<button class='btn btn-link collapsed' data-toggle='collapse' data-target='#collapse_" + disease_trimmed + "' aria-expanded='false' aria-controls='collapse_" + disease_trimmed + "'>" +
+          disease +
+          "</button>" +
+          "</h5>" +
+          "</div>" +
+          "<div id='collapse_" + disease_trimmed + "' class='collapse' aria-labelledby='headingOne' data-parent='#disease_accordion'>" +
+          "<div class='card-body'>" +
+          "<button class='btn btn-secondary button-margin' type='button' data-toggle='collapse' data-target='#control_" + table_id + "' aria-expanded='false'>" +
+          "Options" +
+          "</button>" +
+          "<button class='btn btn-secondary button-margin' type='button' id='expression_" + disease_trimmed + "'>" +
+          "Raw expression data" +
+          "</button>" +
+          "<div class='collapse' id='control_" + table_id + "'>" +
+          "<div class='card card-body'>" +
+          "<div>" +
+          "<p>Set filter for mscor</p>" +
+          "<span>Minimum: </span><input type='text' id='mscore_min_" + table_id + "' name='mscore_min'>&nbsp;" +
+          "<span>Maximum: </span><input type='text' id='mscore_max_" + table_id + "' name='mscore_max'>" +
+          "</div>" +
+          "<hr>" +
+          "<div>" +
+          "<p>Set filter for P-value</p>" +
+          "<span>Minimum: </span><input type='text' id='pvalue_min_" + table_id + "' name='pvalue_min'>&nbsp;" +
+          "<span>Maximum: </span><input type='text' id='pvalue_max_" + table_id + "' name='pvalue_max'>" +
+          "</div>" +
+          "</div>" +
+          "</div>" +
+          "<div class='card-body-table'></div>" +
+          "</div>" +
+          "</div>"
         $('#disease_accordion').append(accordion_card)
-        
+
         let html_table = helper.buildTable(
-          parsed_search_result['diseases'][disease], 
-          table_id, 
+          parsed_search_result['diseases'][disease],
+          table_id,
           Object.keys(parsed_search_result['diseases'][disease][0])
-          )
-        $('#collapse_'+disease_trimmed).find('.card-body-table').html(html_table)
-        
+        )
+        $('#collapse_' + disease_trimmed).find('.card-body-table').html(html_table)
+
         push_interaction_filters(table_id)
-        var table = $("#"+table_id).DataTable({
+        var table = $("#" + table_id).DataTable({
           orderCellsTop: true,
         })
         helper.colSearch(table_id, table)
-        
-        $('#mscore_min_'+table_id+', #mscore_max_'+table_id+', #pvalue_min_'+table_id+', #pvalue_max_'+table_id).keyup(()=>{
+
+        $('#mscore_min_' + table_id + ', #mscore_max_' + table_id + ', #pvalue_min_' + table_id + ', #pvalue_max_' + table_id).keyup(() => {
           table.draw()
         })
         // make rows selectable
-        $('#'+table_id+' tbody').on( 'click', 'tr', function () {
+        $('#' + table_id + ' tbody').on('click', 'tr', function () {
           $(this).toggleClass('selected');
         })
 
@@ -316,7 +314,7 @@ export class SearchComponent implements OnInit {
         } else {
           config['gene_symbol'] = [key_information['gene_symbol']]
         }
-        $('#expression_'+disease_trimmed).click(() => {
+        $('#expression_' + disease_trimmed).click(() => {
           controller.get_expression_ceRNA(config)
         })
       }
