e0438c358b581293ae8809c5303abf54c4349425
Nico Trummer <nictru32@gmail.com>
2024-11-21 23:36:56 +0100

    Re-add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [MatButton, MatToolbar, RouterLink, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'SPONGE-web-frontend';
+  subpages = ['Tutorial', 'Browse', 'Info', 'Download'];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {TutorialComponent} from "./routes/tutorial/tutorial.component";
+import {BrowseComponent} from "./routes/browse/browse.component";
+import {InfoComponent} from "./routes/info/info.component";
+import {DownloadComponent} from "./routes/download/download.component";
+import {HomeComponent} from "./routes/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'tutorial',
+    component: TutorialComponent
+  },
+  {
+    path: 'browse',
+    component: BrowseComponent
+  },
+  {
+    path: 'info',
+    component: InfoComponent
+  },
+  {
+    path: 'download',
+    component: DownloadComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
