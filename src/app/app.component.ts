06f92c1d0d1f1a7e79ef28a123aed361d7168f80
Nico Trummer <nictru32@gmail.com>
2024-11-21 18:16:44 +0100

    Add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import {Component} from '@angular/core';
 import {MatButton} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [MatButton],
+  imports: [MatButton, MatToolbar, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'SPONGE-web-frontend';
+  subpages = [
+    "Tutorial",
+    "Browse",
+    "Info",
+    "Download",
+    "API"
+  ]
 }
