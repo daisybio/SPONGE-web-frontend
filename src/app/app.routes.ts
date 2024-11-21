88d6900b8d638dfce2e84e767a67f7c531e6114d
Nico Trummer <nictru32@gmail.com>
2024-11-21 18:25:44 +0100

    Init subpage routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {TutorialComponent} from "./pages/tutorial/tutorial.component";
+import {BrowseComponent} from "./pages/browse/browse.component";
+import {InfoComponent} from "./pages/info/info.component";
+import {DownloadComponent} from "./pages/download/download.component";
+import {HomeComponent} from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'tutorial',
+    component: TutorialComponent
+  },
+  {
+    path: 'browse',
+    component: BrowseComponent
+  },
+  {
+    path: 'info',
+    component: InfoComponent
+  },
+  {
+    path: 'download',
+    component: DownloadComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
