1af3d1dd5176d12794b8c5b0a21c768a6cae9bc6
Nico Trummer <nictru32@gmail.com>
2024-11-21 17:56:23 +0100

    Init rewrite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,13 @@
-import { Component,Inject } from '@angular/core'
-import { Helper } from 'src/app/helper'
-import { APP_BASE_HREF } from '@angular/common';
-import { SharedService } from './services/shared/shared.service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  /*
-  TS CODE HERE WILL BE EXECUTED IN EVERY COMPONENT
-  */
- constructor(@Inject(APP_BASE_HREF) public baseHref: string, public sharedService: SharedService) {  }
-  title = 'SPONGEdb';
-
-  ngOnInit() {
-
-    const helper = new Helper(this.sharedService)
-    
-    // check cookies 
-    if (helper.getCookie("cookie_accepted") != '1') {
-      // cookies have not been accepted
-      $('#cookie-accept').click( () => {
-        $('.bottom-cookie').addClass('hidden')
-        // set cookie to remember that user has accepted cookies
-        helper.setCookie("cookie_accepted", 1, 100)
-      })
-    } else {
-      // cookies have already been accepted
-      $('.bottom-cookie').addClass('hidden')
-    }
-  }
+  title = 'SPONGE-web-frontend';
 }
--- src/app/interfaces.ts
+++
@@ -1,5 +0,0 @@
-// create interface for igvInput
-export interface IGVInput {
-  hsaList: string[],
-  gene: string
-}
