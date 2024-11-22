06ed5d08794e3914af21f8b9eef575f084bc04c5
Nico Trummer <nictru32@gmail.com>
2024-11-22 10:30:33 +0100

    Add nutshell carousel

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -2,7 +2,9 @@ import {Component} from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
-import {MatButton} from "@angular/material/button";
+import {MatAnchor, MatButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
+import {CarouselComponent, SlideComponent} from "ngx-bootstrap/carousel";
 
 @Component({
   selector: 'app-home',
@@ -11,11 +13,19 @@ import {MatButton} from "@angular/material/button";
     MatInput,
     FormsModule,
     MatLabel,
-    MatButton
+    MatButton,
+    RouterLink,
+    MatAnchor,
+    CarouselComponent,
+    SlideComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   searchGene: string = '';
+
+  imageRoot: string = '/';
+  images: string[] = ['1.svg', '2.png', '3.svg']
+
 }
