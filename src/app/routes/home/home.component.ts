4720a39c6e0d25530632c3f1eca990bbf2507c10
Nico Trummer <nictru32@gmail.com>
2024-11-22 09:11:35 +0100

    Add gene search form

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatFormField,
+    MatInput,
+    FormsModule,
+    MatLabel,
+    MatButton
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  searchGene: string = '';
 }
