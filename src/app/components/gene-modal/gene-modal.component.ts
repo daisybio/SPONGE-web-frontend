75d11a0baac1342ae6e93f58925a88859f191397
Nico Trummer <nictru32@gmail.com>
2024-12-08 10:50:31 +0100

    Make gene ontology searchable

--- src/app/components/gene-modal/gene-modal.component.ts
+++ src/app/components/gene-modal/gene-modal.component.ts
@@ -1,11 +1,16 @@
-import {Component, inject, resource} from '@angular/core';
-import {Gene} from "../../interfaces";
+import {AfterViewInit, Component, effect, inject, model, resource, ViewChild} from '@angular/core';
+import {Gene, GOTerm} from "../../interfaces";
 import {MAT_DIALOG_DATA, MatDialogModule} from "@angular/material/dialog";
 import {MatButtonModule} from "@angular/material/button";
 import {VersionsService} from "../../services/versions.service";
 import {BackendService} from "../../services/backend.service";
 import {MatTabsModule} from "@angular/material/tabs";
 import {MatExpansionModule} from "@angular/material/expansion";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-gene-modal',
@@ -14,15 +19,26 @@ import {MatExpansionModule} from "@angular/material/expansion";
     MatDialogModule,
     MatTabsModule,
     MatExpansionModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   templateUrl: './gene-modal.component.html',
   styleUrl: './gene-modal.component.scss'
 })
-export class GeneModalComponent {
+export class GeneModalComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  columns = ['symbol', 'description']
+  goFilter = model<string>('')
+
   readonly gene = inject<Gene>(MAT_DIALOG_DATA);
   readonly versionsService = inject(VersionsService);
   readonly backend = inject(BackendService);
   readonly version$ = this.versionsService.versionReadOnly();
+
+  goDatasource = new MatTableDataSource<GOTerm>();
 
   readonly geneInfo$ = resource({
     request: this.version$,
@@ -43,4 +59,19 @@ export class GeneModalComponent {
     request: this.version$,
     loader: async (version) => this.backend.getWikiPathways(version.request, this.gene.gene_symbol)
   })
+
+  constructor() {
+    effect(() => {
+      this.goDatasource.data = this.goTerms$.value() ?? [];
+    });
+
+    effect(() => {
+      this.goDatasource.filter = this.goFilter().trim().toLowerCase();
+      console.log(this.goDatasource.filter)
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.goDatasource.paginator = this.paginator;
+  }
 }
