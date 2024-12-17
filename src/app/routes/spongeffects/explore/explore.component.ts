e04c997e2ebdea77d8d4989ab8eb0b4fdce20e43
Nico Trummer <nictru32@gmail.com>
2024-12-17 22:49:45 +0100

    Use tab structure in spongEffects explore

--- src/app/routes/spongeffects/explore/explore.component.ts
+++ src/app/routes/spongeffects/explore/explore.component.ts
@@ -1,11 +1,6 @@
 import {Component} from '@angular/core';
-import {
-  MatExpansionModule,
-  MatExpansionPanel,
-  MatExpansionPanelDescription,
-  MatExpansionPanelTitle
-} from "@angular/material/expansion";
-import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatIconModule} from "@angular/material/icon";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
@@ -13,14 +8,11 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatCardModule} from '@angular/material/card';
 import {ClassPerformancePlotComponent} from "./plots/class-performance-plot/class-performance-plot.component";
 import {OverallAccPlotComponent} from "./plots/overall-acc-plot/overall-acc-plot.component";
+import {MatTabsModule} from "@angular/material/tabs";
 
 @Component({
   selector: 'app-explore',
   imports: [
-    MatExpansionPanel,
-    MatExpansionPanelTitle,
-    MatIcon,
-    MatExpansionPanelDescription,
     MatExpansionModule,
     MatFormFieldModule,
     FormsModule,
@@ -30,7 +22,8 @@ import {OverallAccPlotComponent} from "./plots/overall-acc-plot/overall-acc-plot
     MatIconModule,
     MatCardModule,
     ClassPerformancePlotComponent,
-    OverallAccPlotComponent
+    OverallAccPlotComponent,
+    MatTabsModule
   ],
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.scss', '../spongeffects.component.scss']
