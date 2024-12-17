97b7aacb4474b24a9ab4b65a3c55c927650a3286
Nico Trummer <nictru32@gmail.com>
2024-12-17 20:50:06 +0100

    Update explore/predict switcher

--- src/app/routes/spongeffects/spongeffects.component.ts
+++ src/app/routes/spongeffects/spongeffects.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {Component, model} from '@angular/core';
 import {MatDrawer, MatDrawerContainer, MatDrawerContent} from "@angular/material/sidenav";
-import {MatListItem, MatNavList} from "@angular/material/list";
-import {RouterLink, RouterOutlet} from "@angular/router";
+import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
+import {ExploreComponent} from "./explore/explore.component";
+import {PredictComponent} from "./predict/predict.component";
+
 // import { Tab, Cancer, PlotlyData } from '../../models/spongeffects.model';
 
 @Component({
@@ -11,71 +13,13 @@ import {RouterLink, RouterOutlet} from "@angular/router";
     MatDrawer,
     MatDrawerContainer,
     MatDrawerContent,
-    MatListItem,
-    MatNavList,
-    RouterLink,
-    RouterOutlet
+    MatButtonToggleGroup,
+    MatButtonToggle,
+    ExploreComponent,
+    PredictComponent
   ],
   styleUrls: ['./spongeffects.component.scss']
 })
-export class SpongEffectsComponent implements OnInit, AfterViewInit {
-  // tabs: Tab[] = [
-  //   { value: "explore", viewValue: "Explore", icon: "../../../assets/img/magnifying_glass.png" },
-  //   { value: "predict", viewValue: "Predict", icon: "../../../assets/img/chip-intelligence-processor-svgrepo-com.png" }
-  // ];
-  // selectedTab: Tab = this.tabs[0];
-  // cancers: Cancer[] = [];
-  // selectedCancer: Cancer | undefined = undefined;
-  // sampleDistributionData: PlotlyData | undefined;
-
-  constructor() {}
-
-  ngOnInit(): void {
-    // Initialisierungslogik
-  }
-
-  ngAfterViewInit(): void {
-    // Logik nach dem Laden der Ansicht
-  }
-
-  // setTab(tab: Tab) {
-  //   this.selectedTab = tab;
-  // }
-  //
-  // setPreviewCancer(cancer: Cancer) {
-  //   this.selectedCancer = cancer;
-  //   this.sampleDistributionData = this.getSampleDistributionData();
-  // }
-
-  // getSampleDistributionData(): PlotlyData {
-  //   if (!this.selectedCancer) {
-  //     return { data: [], layout: {}, config: {} };
-  //   }
-  //   return {
-  //     data: [
-  //       {
-  //         values: this.selectedCancer.sampleSizes,
-  //         labels: this.selectedCancer.allSubTypes,
-  //         type: "pie",
-  //         hoverinfo: 'label+value+percent',
-  //         textinfo: 'none'
-  //       }
-  //     ],
-  //     layout: {
-  //       autosize: true,
-  //       showlegend: true,
-  //       legend: { "orientation": "h" },
-  //       margin: { b: 5, l: 5, r: 5, t: 50 },
-  //       title: {
-  //         text: "Sample distribution of " + this.selectedCancer.value + " (" + this.selectedCancer.totalNumberOfSamples() + " samples)",
-  //         font: { size: 14 }
-  //       }
-  //     },
-  //     config: { responsive: true }
-  //   };
-  // }
-
-  onFileSelected(files: File[]) {
-    // Logik für den Datei-Upload
-  }
+export class SpongEffectsComponent {
+  mode = model<'explore' | 'predict'>('explore');
 }
