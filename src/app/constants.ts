2566895b3946fc24384e3cc96cf606f06332c69e
Nico Trummer <nictru32@gmail.com>
2024-12-18 18:14:14 +0100

    Add alternative splicing event type tooltips to modals

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,11 @@
 export const SUBTYPE_DEFAULT = 'Unspecific';
+
+export const AS_DESCRIPTIONS: { [key: string]: string } = {
+  SE: "Skipping Exon",
+  A5: "Alternative 5' Splice Site",
+  A3: "Alternative 3' Splice Site",
+  MX: "Mutually Exclusive Exon",
+  RI: "Retained Intron",
+  AF: "Alternative First Exon",
+  AL: "Alternative Last Exon"
+}
