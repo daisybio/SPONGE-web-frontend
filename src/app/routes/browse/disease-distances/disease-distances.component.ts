e973f3366e7a072d7270073455f676ca28099bd2
Nico Trummer <nictru32@gmail.com>
2024-12-19 13:41:09 +0100

    Implement disease distance scatter

--- src/app/routes/browse/disease-distances/disease-distances.component.ts
+++ src/app/routes/browse/disease-distances/disease-distances.component.ts
@@ -1,73 +1,84 @@
-import {Component, computed, effect, ElementRef, inject, input, OnDestroy, viewChild} from '@angular/core';
+import {Component, computed, effect, ElementRef, inject, input, model, OnDestroy, viewChild} from '@angular/core';
 import {BrowseService} from "../../../services/browse.service";
 import {capitalize} from "lodash";
+import {MatButtonToggle, MatButtonToggleGroup} from "@angular/material/button-toggle";
 
 declare const Plotly: any;
 
 @Component({
   selector: 'app-disease-distances',
-  imports: [],
+  imports: [
+    MatButtonToggleGroup,
+    MatButtonToggle
+  ],
   templateUrl: './disease-distances.component.html',
   styleUrl: './disease-distances.component.scss'
 })
 export class DiseaseDistancesComponent implements OnDestroy {
   browseService = inject(BrowseService);
-
+  mode$ = model<'scatter' | 'heatmap'>('scatter');
   refreshSignal = input.required<any>();
-  scatterDiv$ = viewChild.required<ElementRef<HTMLDivElement>>('scatter');
+  plotDiv$ = viewChild.required<ElementRef<HTMLDivElement>>('plot');
   heatmapDiv$ = viewChild.required<ElementRef<HTMLDivElement>>('heatmap');
 
   data$ = this.browseService.networkResults$;
-  scatterData$ = computed(() => {
+  plotData$ = computed(() => {
     const data = this.data$();
     if (!data) return;
-    return data.type.euclidean_distances;
+    const mode = this.mode$();
+    if (mode == 'scatter') {
+      const scatterData = data.type.euclidean_distances;
+
+      return {
+        x: scatterData.x,
+        y: scatterData.y,
+        type: 'scatter',
+        mode: 'markers',
+        text: scatterData.labels.map(label => capitalize(label))
+      };
+    } else {
+      const heatmapData = data.type.scores;
+
+      return {
+        z: heatmapData.values,
+        type: 'heatmap',
+        x: heatmapData.labels.map(label => capitalize(label)),
+        y: heatmapData.labels.map(label => capitalize(label)),
+      }
+    }
   });
-  heatmapData$ = computed(() => {
-    const data = this.data$();
-    if (!data) return;
-    return data.type.scores;
-  });
 
 
-  updateScatter = effect(() => {
-    const scatterData = this.scatterData$();
-    if (!scatterData) return;
-    const div = this.scatterDiv$().nativeElement;
+  updatePlot = effect(() => {
+    const plotData = this.plotData$();
+    if (!plotData) return;
+    const div = this.plotDiv$().nativeElement;
 
-    Plotly.newPlot(div, [{
-      x: scatterData.x,
-      y: scatterData.y,
-      type: 'scatter',
-      mode: 'markers',
-      text: scatterData.labels.map(label => capitalize(label)),
-    }], {
-      margin: {t: 0, l: 0, r: 0, b: 0},
-      height: 500,
+    Plotly.newPlot(div, [plotData], {
+      height: 700,
+      yaxis: {automargin: true},
+      xaxis: {automargin: true},
     });
   });
 
   refreshEffect = effect(() => {
     this.refreshSignal();
-
     this.refresh();
   });
 
   refresh() {
-    const divs = [this.scatterDiv$().nativeElement, this.heatmapDiv$().nativeElement];
+    const div = this.plotDiv$().nativeElement;
 
-    for (const div of divs) {
-      if (div.checkVisibility()) {
-        Plotly.Plots.resize(div);
-      }
+    if (div.checkVisibility()) {
+      Plotly.Plots.resize(div);
     }
   }
 
   ngOnDestroy() {
-    Plotly.purge(this.scatterDiv$().nativeElement);
+    Plotly.purge(this.plotDiv$().nativeElement);
     Plotly.purge(this.heatmapDiv$().nativeElement);
 
     this.refreshEffect.destroy();
-    this.updateScatter.destroy();
+    this.updatePlot.destroy();
   }
 }
