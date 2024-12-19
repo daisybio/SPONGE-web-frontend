8f814164b7ddc90d9a170943cc1658245e39b261
Nico Trummer <nictru32@gmail.com>
2024-12-19 19:56:19 +0100

    Re-implement SpongEffects predict file upload

--- src/app/routes/spongeffects/predict/form/predict-form.component.ts
+++ src/app/routes/spongeffects/predict/form/predict-form.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Component, computed} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatExpansionModule} from "@angular/material/expansion";
@@ -7,7 +7,10 @@ import {MatCheckbox} from "@angular/material/checkbox";
 import {MatOption} from "@angular/material/core";
 import {MatSelect} from "@angular/material/select";
 import {NgForOf} from "@angular/common";
-import {MatButton} from "@angular/material/button";
+import {MatAnchor, MatButton} from "@angular/material/button";
+import {MatDropzone} from "@ngx-dropzone/material";
+import {FileInputDirective} from "@ngx-dropzone/cdk";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-predict-form',
@@ -22,7 +25,11 @@ import {MatButton} from "@angular/material/button";
     MatOption,
     MatSelect,
     NgForOf,
-    MatButton
+    MatButton,
+    FormsModule,
+    MatAnchor,
+    MatDropzone,
+    FileInputDirective
   ],
   templateUrl: './predict-form.component.html',
   styleUrl: './predict-form.component.scss'
@@ -30,6 +37,7 @@ import {MatButton} from "@angular/material/button";
 export class PredictFormComponent {
   methods = ['gsva', 'ssgsea', 'OE']
   formGroup = new FormGroup({
+    useExampleExpression: new FormControl<boolean>(false),
     mscor: new FormControl(0.1, [Validators.min(0), Validators.max(1)]),
     fdr: new FormControl(0.05, [Validators.min(0), Validators.max(1)]),
     minSize: new FormControl(100, [Validators.min(0)]),
@@ -39,4 +47,7 @@ export class PredictFormComponent {
     logScaling: new FormControl<boolean>(true),
     predictSubtypes: new FormControl<boolean>(false),
   });
+
+  query$ = toSignal(this.formGroup.valueChanges);
+  useExampleExpression$ = computed(() => this.query$()?.useExampleExpression);
 }
