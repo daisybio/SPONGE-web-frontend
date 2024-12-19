a31a98a030868ed7fc508d2479714f7e013ee6e2
Nico Trummer <nictru32@gmail.com>
2024-12-19 19:28:08 +0100

    Re-implement SpongEffects predict form in drawer

--- src/app/routes/spongeffects/predict/form/predict-form.component.ts
+++ src/app/routes/spongeffects/predict/form/predict-form.component.ts
@@ -1,11 +1,42 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatOption} from "@angular/material/core";
+import {MatSelect} from "@angular/material/select";
+import {NgForOf} from "@angular/common";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-predict-form',
-  imports: [],
+  imports: [
+    MatError,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    MatExpansionModule,
+    MatCheckbox,
+    MatOption,
+    MatSelect,
+    NgForOf,
+    MatButton
+  ],
   templateUrl: './predict-form.component.html',
   styleUrl: './predict-form.component.scss'
 })
 export class PredictFormComponent {
-
+  methods = ['gsva', 'ssgsea', 'OE']
+  formGroup = new FormGroup({
+    mscor: new FormControl(0.1, [Validators.min(0), Validators.max(1)]),
+    fdr: new FormControl(0.05, [Validators.min(0), Validators.max(1)]),
+    minSize: new FormControl(100, [Validators.min(0)]),
+    maxSize: new FormControl(2000, [Validators.min(0)]),
+    minExpr: new FormControl(10, [Validators.min(0)]),
+    method: new FormControl(this.methods[0]),
+    logScaling: new FormControl<boolean>(true),
+    predictSubtypes: new FormControl<boolean>(false),
+  });
 }
