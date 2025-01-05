671ae83b3af8d1df43946f981b1aa00b5ae04bc2
strasserle <strasser.lena@gmx.de>
2025-01-05 22:51:10 +0100

    spongeffects service has now dataset object

--- src/app/services/spong-effects.service.ts
+++ src/app/services/spong-effects.service.ts
@@ -1,7 +1,8 @@
-import {computed, inject, Injectable, resource} from '@angular/core';
+import {computed, inject, Injectable, resource, linkedSignal} from '@angular/core';
 import {BackendService} from "./backend.service";
 import {VersionsService} from "./versions.service";
-import {SpongEffectsRun} from "../interfaces";
+import {SpongEffectsRun, Dataset} from "../interfaces";
+import { Data } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,22 @@ import {SpongEffectsRun} from "../interfaces";
 export class SpongEffectsService {
   backend = inject(BackendService);
   versionsService = inject(VersionsService);
+  datasets$ = computed(() => {
+    const runs = this.spongEffectsRuns$.value() || [];
+    const datasets = runs.map((run: SpongEffectsRun) => ({
+      dataset_ID: run.dataset_ID,
+      disease_name: run.disease_name,
+      data_origin: run.data_origin,
+      disease_subtype: run.disease_subtype,
+      disease_type: run.disease_type,
+      download_url: run.download_url,
+      sponge_db_version: run.sponge_db_version,
+    } as Dataset));
+    return datasets.filter((dataset, index, self) =>
+      index === self.findIndex((d) => d.dataset_ID === dataset.dataset_ID)
+    );
+  });
+  // diseaseNames$ = linkedSignal(() => this.datasets$().map(d => d.disease_name));
   diseaseNames$ = computed(() => {
     const runs = this.spongEffectsRuns$.value() || [];
     return runs.map((run: SpongEffectsRun) => run.disease_name)
