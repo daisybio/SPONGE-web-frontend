95d4cabe843da4cf89d080f596c302ad78883dbf
nictru <nictru32@gmail.com>
2025-01-10 21:36:46 +0100

    Move predicting to dedicated service

--- src/app/routes/spongeffects/predict/form/predict-form.component.ts
+++ src/app/routes/spongeffects/predict/form/predict-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, Signal, ViewChild } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -17,16 +17,15 @@ import { MatButton } from '@angular/material/button';
 import { MatDropzone } from '@ngx-dropzone/material';
 import { FileInputDirective } from '@ngx-dropzone/cdk';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { ExampleExpression, PredictCancerType } from '../../../../interfaces';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
-import { BackendService } from '../../../../services/backend.service';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { PredictionResultsComponent } from '../prediction-results/prediction-results.component';
 import { SPONGE_EXAMPLE_URL } from '../../../../constants';
 import { MatDialog } from '@angular/material/dialog';
 import { ExampleFileModalComponent } from './example-file-modal/example-file-modal.component';
+import { PredictService } from '../service/predict.service';
+import { VersionsService } from '../../../../services/versions.service';
 
 @Component({
   selector: 'app-predict-form',
@@ -59,139 +58,64 @@ export class PredictFormComponent {
   methods = ['gsva', 'ssgsea', 'OE'];
   formGroup = new FormGroup({
     useExampleExpression: new FormControl<boolean>(false),
-    mscor: new FormControl(0.1, [Validators.min(0), Validators.max(1)]),
-    fdr: new FormControl(0.05, [Validators.min(0), Validators.max(1)]),
-    minSize: new FormControl(100, [Validators.min(0)]),
-    maxSize: new FormControl(2000, [Validators.min(0)]),
-    minExpr: new FormControl(10, [Validators.min(0)]),
-    method: new FormControl(this.methods[0]),
-    logScaling: new FormControl<boolean>(true),
-    predictSubtypes: new FormControl<boolean>(false),
+    mscor: new FormControl<number>(0.1, {
+      nonNullable: true,
+      validators: [Validators.min(0), Validators.max(1)],
+    }),
+    fdr: new FormControl(0.05, {
+      nonNullable: true,
+      validators: [Validators.min(0), Validators.max(1)],
+    }),
+    minSize: new FormControl(100, {
+      nonNullable: true,
+      validators: [Validators.min(0)],
+    }),
+    maxSize: new FormControl(2000, {
+      nonNullable: true,
+      validators: [Validators.min(0)],
+    }),
+    minExpr: new FormControl(10, {
+      nonNullable: true,
+      validators: [Validators.min(0)],
+    }),
+    method: new FormControl(this.methods[0], { nonNullable: true }),
+    logScaling: new FormControl<boolean>(true, { nonNullable: true }),
+    predictSubtypes: new FormControl<boolean>(false, { nonNullable: true }),
   });
   fileCtrl = new FormControl<File | null>(null);
   fileCtrlValue$ = toSignal(this.fileCtrl.valueChanges);
-  backend = inject(BackendService);
   dialog = inject(MatDialog);
+  predictService = inject(PredictService);
 
-  predictionLoading: boolean = false;
-  @ViewChild(PredictionResultsComponent)
-  predictionResultsComponent!: PredictionResultsComponent;
+  isLoading$ = this.predictService.isLoading$;
   query$ = toSignal(this.formGroup.valueChanges);
   useExampleExpression$ = computed(
     () => this.query$()?.useExampleExpression || false,
   );
-  showExpressionExample = false;
-  prediction: Signal<PredictCancerType> | undefined;
-  exampleExpressionData: MatTableDataSource<any> =
-    new MatTableDataSource<ExampleExpression>();
-  exampleData: ExampleExpression[] = [];
-  exampleDataKeys: (keyof ExampleExpression)[] = [];
 
-  exampleDataFile = async () => {
+  versionService = inject(VersionsService);
+
+  exampleDataFile = (async () => {
     const response = await fetch(SPONGE_EXAMPLE_URL);
     const blob = await response.blob();
     return new File([blob], 'example.csv', { type: 'text/csv' });
-  };
+  })();
 
   selectedExpressionFile$ = computed(async () => {
     if (this.useExampleExpression$()) {
-      return await this.exampleDataFile();
+      return await this.exampleDataFile;
     } else {
       return this.fileCtrlValue$() || undefined;
     }
   });
 
-  getValue(row: ExampleExpression, key: keyof ExampleExpression): any {
-    return row[key];
-  }
-
-  /*
-    async predict() {
-      this.predictionQueried = true;
-      this.predictionLoading = true;
-      // start timer of estimated run time
-      // this.startTimer().then(_ => this.timerRunning = false);
-      // start workflow
-      const prediction = await this.getPredictionData();
-      this.prediction = signal(prediction);
-
-      // .then(data => this.processPredictions(data))
-      this.predictionLoading = false;
-      console.log(this.prediction);
-    }
-
-
-    async getPredictionData(): Promise<PredictCancerType> {
-      let uploadedFile: Blob;
-      if (this.formGroup.get('useExampleExpression')!.value === true) {
-        // const exampleDataString = Papa.unparse(this.exampleData);
-        // const blob = new Blob([exampleDataString], { type: 'text/csv' });
-        // uploadedFile = new File([blob], 'GSE123845_exp_tpm_matrix_processed.csv', { type: 'text/csv', lastModified: new Date().getTime() });
-        const response = await fetch(this.csvFilePath);
-        const blob = await response.blob();
-        uploadedFile = new File(
-          [blob],
-          'GSE123845_exp_tpm_matrix_processed.csv',
-          {
-            type: 'text/csv',
-            lastModified: new Date().getTime(),
-          },
-        );
-      } else {
-        uploadedFile = this.fileCtrl.value;
-      }
-      console.log(uploadedFile);
-      // send file and parameters to API and return response
-      if (
-        this.formGroup.value.predictSubtypes === undefined ||
-        this.formGroup.value.logScaling === undefined ||
-        this.formGroup.value.mscor === undefined ||
-        this.formGroup.value.fdr === undefined ||
-        this.formGroup.value.minSize === undefined ||
-        this.formGroup.value.maxSize === undefined ||
-        this.formGroup.value.minExpr === undefined ||
-        this.formGroup.value.method === undefined
-      ) {
-        return {
-          meta: {
-            runtime: 0,
-            level: '',
-            n_samples: 0,
-            type_predict: '',
-            subtype_predict: '',
-          },
-          data: [],
-        };
-      }
-      const prediction = this.backend.predictCancerType(
-        this.versionService.versionReadOnly()(),
-        uploadedFile,
-        this.formGroup.value.predictSubtypes!,
-        this.formGroup.value.logScaling!,
-        this.formGroup.value.mscor!,
-        this.formGroup.value.fdr!,
-        this.formGroup.value.minSize!,
-        this.formGroup.value.maxSize!,
-        this.formGroup.value.minExpr!,
-        this.formGroup.value.method!,
-      );
-
-      console.log(prediction);
-      return prediction;
-    }
-     */
-
-  runButtonDisabled(): boolean {
-    return (
-      !(
-        this.formGroup.get('useExampleExpression')?.value ||
-        this.expressionUploaded()
-      ) || this.predictionLoading
-    );
-  }
-
-  expressionUploaded(): boolean {
-    return this.fileCtrl.value !== null;
+  async predict() {
+    const query = this.formGroup.getRawValue();
+    this.predictService.request({
+      file: (await this.selectedExpressionFile$()) as File,
+      version: this.versionService.versionReadOnly()(),
+      ...query,
+    });
   }
 
   async showExpressionFile(file: File) {
--- src/app/routes/spongeffects/predict/service/predict.service.ts
+++ src/app/routes/spongeffects/predict/service/predict.service.ts
@@ -1,8 +1,64 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, resource, signal } from '@angular/core';
+import { BackendService } from '../../../../services/backend.service';
+
+export interface Query {
+  file: File;
+  mscor: number;
+  fdr: number;
+  minSize: number;
+  maxSize: number;
+  minExpr: number;
+  method: string;
+  logScaling: boolean;
+  predictSubtypes: boolean;
+  version: number;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class PredictService {
+  backend = inject(BackendService);
+  private readonly _query$ = signal<Query | undefined>(undefined);
+
+  prediction$ = resource({
+    request: this._query$,
+    loader: (param) => {
+      const query = param.request;
+      if (!query) {
+        return Promise.resolve({
+          meta: {
+            runtime: 0,
+            level: '',
+            n_samples: 0,
+            type_predict: '',
+            subtype_predict: '',
+          },
+          data: [],
+        });
+      }
+      return this.backend.predictCancerType(
+        query.version,
+        query.file,
+        query.predictSubtypes,
+        query.logScaling,
+        query.mscor,
+        query.fdr,
+        query.minSize,
+        query.maxSize,
+        query.minExpr,
+        query.method,
+      );
+    },
+  });
+
   constructor() {}
+
+  public get isLoading$() {
+    return this.prediction$.isLoading;
+  }
+
+  request(query: Query) {
+    this._query$.set(query);
+  }
 }
