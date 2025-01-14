cecefdafe82068f3d9c26d8efb7ce06bf0533326
strasserle <strasser.lena@gmx.de>
2025-01-14 16:30:43 +0100

    example expression preview v2

--- src/app/routes/spongeffects/predict/form/example-file-modal/example-file-modal.component.ts
+++ src/app/routes/spongeffects/predict/form/example-file-modal/example-file-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit, Signal, ViewChild, signal, resource, computed, effect } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
@@ -6,6 +6,7 @@ import Papa from 'papaparse';
 import { config } from 'rxjs';
 import { ExampleExpression } from '../../../../../interfaces';
 import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @Component({
@@ -13,26 +14,55 @@ import { CommonModule } from '@angular/common';
   imports: [
     MatPaginatorModule,
     MatTableModule,
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './example-file-modal.component.html',
   styleUrl: './example-file-modal.component.scss',
 })
 export class ExampleFileModalComponent implements OnInit {
   dialog = inject(MatDialog);
+  isLoading = false;
   readonly file = inject<File>(MAT_DIALOG_DATA);
   readonly content = (async () => {
     return Papa.parse(await this.file.text(), {'header': true});
   })();
 
   dataSource: MatTableDataSource<unknown, MatPaginator> = new MatTableDataSource<unknown, MatPaginator>([]);
+  dataSourceResource = resource({
+    request: computed(() => {
+      return {
+        data: this.content
+      }
+    }),
+    loader: async (param) => {
+      const data = await param.request.data;
+      return new MatTableDataSource(data.data);
+    }
+    
+  })
+
   columns: string[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor() {
+    effect(() => {
+      if (this.dataSourceResource.value()) {
+        this.dataSourceResource.value()!.paginator = this.paginator;
+        this.dataSource = this.dataSourceResource.value()!;
+      }
+    });
+  }
 
   ngOnInit() {
     this.content.then((res) => {
-      console.log(res);
-      this.dataSource = new MatTableDataSource(res.data);
+      // this.dataSource = signal(new MatTableDataSource(res.data));
       this.columns = res.meta.fields!;
+      // this.dataSource.value().paginator = this.paginator;
+      this.isLoading = false;
     });
   }
+
+
 }
