76b3e3e35f621996f8220f61c51ea5f647c7683c
nictru <nictru32@gmail.com>
2025-01-16 14:38:01 +0100

    Replace "Node" with active level across browse

--- src/app/components/interactions-table/interactions-table.component.ts
+++ src/app/components/interactions-table/interactions-table.component.ts
@@ -1,15 +1,28 @@
-import {AfterViewInit, Component, computed, inject, input, ViewChild} from '@angular/core';
-import {Gene, GeneInteraction, Transcript, TranscriptInteraction} from "../../interfaces";
-import {MatTableDataSource, MatTableModule} from "@angular/material/table";
-import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
-import {MatSort, MatSortHeader} from "@angular/material/sort";
-import {GeneModalComponent} from "../gene-modal/gene-modal.component";
-import {MatDialog} from "@angular/material/dialog";
-import {MatButton} from "@angular/material/button";
-import {MatTooltip} from "@angular/material/tooltip";
-import {BrowseService} from "../../services/browse.service";
-import {TranscriptModalComponent} from "../transcript-modal/transcript-modal.component";
-import {InteractionModalComponent} from "../interaction-modal/interaction-modal.component";
+import {
+  AfterViewInit,
+  Component,
+  computed,
+  inject,
+  input,
+  ViewChild,
+} from '@angular/core';
+import {
+  Gene,
+  GeneInteraction,
+  Transcript,
+  TranscriptInteraction,
+} from '../../interfaces';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortHeader } from '@angular/material/sort';
+import { GeneModalComponent } from '../gene-modal/gene-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatButton } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
+import { BrowseService } from '../../services/browse.service';
+import { TranscriptModalComponent } from '../transcript-modal/transcript-modal.component';
+import { InteractionModalComponent } from '../interaction-modal/interaction-modal.component';
+import { capitalize } from 'lodash';
 
 @Component({
   selector: 'app-interactions-table',
@@ -19,34 +32,44 @@ import {InteractionModalComponent} from "../interaction-modal/interaction-modal.
     MatSortHeader,
     MatSort,
     MatButton,
-    MatTooltip
+    MatTooltip,
   ],
   templateUrl: './interactions-table.component.html',
-  styleUrl: './interactions-table.component.scss'
+  styleUrl: './interactions-table.component.scss',
 })
 export class InteractionsTableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  level$ = input<'gene' | 'transcript'>();
   interactions$ = input<(GeneInteraction | TranscriptInteraction)[]>();
   readonly dialog = inject(MatDialog);
 
-  columns = ["name_1", "name_2", "mirna", "correlation", "mscor", "padj"];
+  columns = ['name_1', 'name_2', 'mirna', 'correlation', 'mscor', 'padj'];
 
   dataSource$ = computed(() => {
-    return new MatTableDataSource((this.interactions$() || []).map(interaction => {
-      const names = BrowseService.getInteractionFullNames(interaction);
-      return {
-        name_1: names[0],
-        name_2: names[1],
-        correlation: interaction.correlation,
-        mscor: interaction.mscor,
-        padj: interaction.p_value,
-        obj1: 'gene1' in interaction ? interaction.gene1 : interaction.transcript_1,
-        obj2: 'gene2' in interaction ? interaction.gene2 : interaction.transcript_2,
-        interaction
-      }
-    }));
+    return new MatTableDataSource(
+      (this.interactions$() || []).map((interaction) => {
+        const names = BrowseService.getInteractionFullNames(interaction);
+        return {
+          name_1: names[0],
+          name_2: names[1],
+          correlation: interaction.correlation,
+          mscor: interaction.mscor,
+          padj: interaction.p_value,
+          obj1:
+            'gene1' in interaction
+              ? interaction.gene1
+              : interaction.transcript_1,
+          obj2:
+            'gene2' in interaction
+              ? interaction.gene2
+              : interaction.transcript_2,
+          interaction,
+        };
+      }),
+    );
   });
+  protected readonly capitalize = capitalize;
 
   ngAfterViewInit(): void {
     const dataSource = this.dataSource$();
@@ -56,19 +79,19 @@ export class InteractionsTableComponent implements AfterViewInit {
 
   openMiRNADialog(interaction: GeneInteraction | TranscriptInteraction) {
     this.dialog.open(InteractionModalComponent, {
-      data: interaction
-    })
+      data: interaction,
+    });
   }
 
   openDialog(entity: Gene | Transcript) {
     if ('ensg_number' in entity) {
       this.dialog.open(GeneModalComponent, {
-        data: entity
-      })
+        data: entity,
+      });
     } else {
       this.dialog.open(TranscriptModalComponent, {
-        data: entity
-      })
+        data: entity,
+      });
     }
   }
 }
--- src/app/routes/browse/browse.component.ts
+++ src/app/routes/browse/browse.component.ts
@@ -1,23 +1,24 @@
-import {Component, computed, inject, signal} from '@angular/core';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatTabsModule} from "@angular/material/tabs";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatExpansionModule} from "@angular/material/expansion";
-import {FormComponent} from "./form/form.component";
-import {InteractionsComponent} from "./interactions/interactions.component";
-import {NetworkComponent} from "./network/network.component";
-import {HeatmapComponent} from "./heatmap/heatmap.component";
-import {BrowseService} from "../../services/browse.service";
-import {SurvivalAnalysisComponent} from "./survival-analysis/survival-analysis.component";
-import {ActiveEntitiesComponent} from "./active-entities/active-entities.component";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatIcon} from "@angular/material/icon";
-import {MatAnchor} from "@angular/material/button";
-import {VersionsService} from "../../services/versions.service";
-import {NodesComponent} from "./nodes/nodes.component";
-import {GSEAComponent} from "./gsea/gsea.component";
-import {DiseaseSimilarityComponent} from "./disease-distances/disease-similarity.component";
-import {fromEvent} from "rxjs";
+import { Component, computed, inject, signal } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FormComponent } from './form/form.component';
+import { InteractionsComponent } from './interactions/interactions.component';
+import { NetworkComponent } from './network/network.component';
+import { HeatmapComponent } from './heatmap/heatmap.component';
+import { BrowseService } from '../../services/browse.service';
+import { SurvivalAnalysisComponent } from './survival-analysis/survival-analysis.component';
+import { ActiveEntitiesComponent } from './active-entities/active-entities.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIcon } from '@angular/material/icon';
+import { MatAnchor } from '@angular/material/button';
+import { VersionsService } from '../../services/versions.service';
+import { NodesComponent } from './nodes/nodes.component';
+import { GSEAComponent } from './gsea/gsea.component';
+import { DiseaseSimilarityComponent } from './disease-distances/disease-similarity.component';
+import { fromEvent } from 'rxjs';
+import { capitalize } from 'lodash';
 
 @Component({
   selector: 'app-browse',
@@ -37,10 +38,10 @@ import {fromEvent} from "rxjs";
     MatAnchor,
     NodesComponent,
     GSEAComponent,
-    DiseaseSimilarityComponent
+    DiseaseSimilarityComponent,
   ],
   templateUrl: './browse.component.html',
-  styleUrl: './browse.component.scss'
+  styleUrl: './browse.component.scss',
 })
 export class BrowseComponent {
   refresh$ = signal(0);
@@ -51,12 +52,17 @@ export class BrowseComponent {
   hasData$ = computed(() => this.browseService.nodes$().length > 0);
   isLoading$ = this.browseService.isLoading$;
   rawDataURL$ = this.browseService.rawDataURL();
-  hasNetworkResults$ = computed(() => this.browseService.networkResults$() !== undefined);
-  hasGseaContrasts$ = computed(() => this.browseService.possibleComparisons$().length > 0);
+  hasNetworkResults$ = computed(
+    () => this.browseService.networkResults$() !== undefined,
+  );
+  hasGseaContrasts$ = computed(
+    () => this.browseService.possibleComparisons$().length > 0,
+  );
+  protected readonly capitalize = capitalize;
 
   constructor() {
     fromEvent(window, 'resize').subscribe(() => this.refresh());
   }
 
-  refresh = () => this.refresh$.update(v => v + 1);
+  refresh = () => this.refresh$.update((v) => v + 1);
 }
