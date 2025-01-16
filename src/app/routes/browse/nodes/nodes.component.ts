313138e6089e8714a95167fd58b82e886397e4cb
nictru <nictru32@gmail.com>
2025-01-16 15:27:06 +0100

    Add info to nodes table in browse view

--- src/app/routes/browse/nodes/nodes.component.ts
+++ src/app/routes/browse/nodes/nodes.component.ts
@@ -1,38 +1,54 @@
-import {AfterViewInit, Component, inject, ViewChild} from '@angular/core';
-import {BrowseService} from "../../../services/browse.service";
-import {Gene, GeneNode, Transcript, TranscriptNode} from "../../../interfaces";
-import {MatTableDataSource, MatTableModule} from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatSort, MatSortHeader} from "@angular/material/sort";
-import {MatButton} from "@angular/material/button";
-import {MatDialog} from "@angular/material/dialog";
-import {GeneModalComponent} from "../../../components/gene-modal/gene-modal.component";
-import {MatTooltip} from "@angular/material/tooltip";
-import {TranscriptModalComponent} from "../../../components/transcript-modal/transcript-modal.component";
+import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
+import { BrowseService } from '../../../services/browse.service';
+import {
+  Gene,
+  GeneNode,
+  Transcript,
+  TranscriptNode,
+} from '../../../interfaces';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, MatSortHeader } from '@angular/material/sort';
+import { MatButton } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { GeneModalComponent } from '../../../components/gene-modal/gene-modal.component';
+import { MatTooltip } from '@angular/material/tooltip';
+import { TranscriptModalComponent } from '../../../components/transcript-modal/transcript-modal.component';
+import { InfoComponent } from '../../../components/info/info.component';
 
 @Component({
   selector: 'app-nodes',
-  imports: [MatTableModule, MatPaginator, MatSort, MatSortHeader, MatButton, MatTooltip],
+  imports: [
+    MatTableModule,
+    MatPaginator,
+    MatSort,
+    MatSortHeader,
+    MatButton,
+    MatTooltip,
+    InfoComponent,
+  ],
   templateUrl: './nodes.component.html',
-  styleUrl: './nodes.component.scss'
+  styleUrl: './nodes.component.scss',
 })
 export class NodesComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  columns = ["identifier", "betweenness", "eigenvector", "node_degree"];
+  columns = ['identifier', 'betweenness', 'eigenvector', 'node_degree'];
   dataSource: MatTableDataSource<GeneNode | TranscriptNode>;
   readonly dialog = inject(MatDialog);
 
   constructor(browseService: BrowseService) {
-    this.dataSource = new MatTableDataSource<any>(browseService.nodes$().map(node => {
-      return {
-        identifier: BrowseService.getNodeFullName(node),
-        betweenness: node.betweenness,
-        eigenvector: node.eigenvector,
-        node_degree: node.node_degree,
-        obj: 'gene' in node ? node.gene : node.transcript
-      }
-    }));
+    this.dataSource = new MatTableDataSource<any>(
+      browseService.nodes$().map((node) => {
+        return {
+          identifier: BrowseService.getNodeFullName(node),
+          betweenness: node.betweenness,
+          eigenvector: node.eigenvector,
+          node_degree: node.node_degree,
+          obj: 'gene' in node ? node.gene : node.transcript,
+        };
+      }),
+    );
   }
 
   ngAfterViewInit() {
@@ -43,12 +59,12 @@ export class NodesComponent implements AfterViewInit {
   openDialog(entity: Gene | Transcript) {
     if ('ensg_number' in entity) {
       this.dialog.open(GeneModalComponent, {
-        data: entity
-      })
+        data: entity,
+      });
     } else {
       this.dialog.open(TranscriptModalComponent, {
-        data: entity
-      })
+        data: entity,
+      });
     }
   }
 }
