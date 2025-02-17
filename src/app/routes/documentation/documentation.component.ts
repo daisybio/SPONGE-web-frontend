5ae84c85ed9172f7b4bcdc1baeb7cf108582dee4
David Wagemann <dawag2001@gmail.com>
2025-02-17 12:07:32 +0100

    v2 documentation

--- src/app/routes/documentation/documentation.component.ts
+++ src/app/routes/documentation/documentation.component.ts
@@ -1,7 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, inject, resource} from '@angular/core';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListItem, MatNavList} from "@angular/material/list";
 import {RouterLink, RouterOutlet} from "@angular/router";
+import { VersionsService } from '../../services/versions.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-documentation',
@@ -10,11 +12,21 @@ import {RouterLink, RouterOutlet} from "@angular/router";
     MatListItem,
     MatSidenavModule,
     RouterOutlet,
-    RouterLink
+    RouterLink,
   ],
   templateUrl: './documentation.component.html',
   styleUrl: './documentation.component.scss'
 })
 export class DocumentationComponent {
+    versionService = inject(VersionsService);
+    version$ = this.versionService.versionReadOnly();
 
+    constructor(private router: Router) {}
+    
+    version_change = resource({
+        request: this.version$,
+        loader: async () => {
+            this.router.navigate(["/documentation/"]);
+        }
+    });
 }
