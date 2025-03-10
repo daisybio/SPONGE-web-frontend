9ed582e19181042dd6395d6cbfdadea1df2736e6
nictru <nictru32@gmail.com>
2025-03-10 14:17:42 +0100

    Fix v1 survival analysis

--- src/app/routes/browse/survival-analysis/kmplot/kmplot.component.ts
+++ src/app/routes/browse/survival-analysis/kmplot/kmplot.component.ts
@@ -1,11 +1,22 @@
-import {Component, computed, effect, ElementRef, input, OnInit, resource, ResourceRef, ViewChild} from '@angular/core';
-import {Dataset, Gene, GeneNode, SurvivalRate} from "../../../../interfaces";
-import {BackendService} from "../../../../services/backend.service";
-import {MatCardModule} from "@angular/material/card";
-import {compute} from "@fullstax/kaplan-meier-estimator";
-import {VersionsService} from "../../../../services/versions.service";
-import {ReplaySubject} from "rxjs";
-import {MatProgressBar} from "@angular/material/progress-bar";
+import {
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  input,
+  OnInit,
+  resource,
+  ResourceRef,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
+import { Dataset, Gene, GeneNode, SurvivalRate } from '../../../../interfaces';
+import { BackendService } from '../../../../services/backend.service';
+import { MatCardModule } from '@angular/material/card';
+import { compute } from '@fullstax/kaplan-meier-estimator';
+import { VersionsService } from '../../../../services/versions.service';
+import { ReplaySubject } from 'rxjs';
+import { MatProgressBar } from '@angular/material/progress-bar';
 
 declare const Plotly: any;
 
@@ -19,35 +30,46 @@ interface CombinedPlotData {
 
 @Component({
   selector: 'app-kmplot',
-  imports: [
-    MatCardModule,
-    MatProgressBar
-  ],
+  imports: [MatCardModule, MatProgressBar],
   templateUrl: './kmplot.component.html',
-  styleUrl: './kmplot.component.scss'
+  styleUrl: './kmplot.component.scss',
 })
-export class KMPlotComponent implements OnInit {
-  gene = input.required<GeneNode>()
-  disease = input.required<Dataset>()
-  @ViewChild("plot") plot!: ElementRef;
+export class KMPlotComponent {
+  gene = input.required<GeneNode>();
+  disease = input.required<Dataset>();
+  plot = viewChild<ElementRef<HTMLDivElement>>('plot');
+  refresh$ = input<any>(0, { alias: 'refresh' });
 
   plotDataResource: ResourceRef<CombinedPlotData | undefined>;
-  plotData = new ReplaySubject<CombinedPlotData | undefined>();
 
-  constructor(private backend: BackendService, versionsService: VersionsService) {
+  constructor(
+    private backend: BackendService,
+    versionsService: VersionsService
+  ) {
     this.plotDataResource = resource({
       request: computed(() => {
         return {
           gene: this.gene().gene,
           disease: this.disease(),
           version: versionsService.versionReadOnly()(),
-        }
+        };
       }),
       loader: async (param) => {
-        const pVals$ = this.backend.getSurvivalPValues(param.request.version, [param.request.gene.ensg_number], param.request.disease);
-        const surivialRates$ = this.backend.getSurvivalRates(param.request.version, [param.request.gene.ensg_number], param.request.disease);
+        const pVals$ = this.backend.getSurvivalPValues(
+          param.request.version,
+          [param.request.gene.ensg_number],
+          param.request.disease
+        );
+        const surivialRates$ = this.backend.getSurvivalRates(
+          param.request.version,
+          [param.request.gene.ensg_number],
+          param.request.disease
+        );
 
-        const [pVals, survivalRates] = await Promise.all([pVals$, surivialRates$]);
+        const [pVals, survivalRates] = await Promise.all([
+          pVals$,
+          surivialRates$,
+        ]);
 
         if (pVals.length === 0) {
           return undefined;
@@ -55,21 +77,47 @@ export class KMPlotComponent implements OnInit {
 
         const pValue = pVals[0].pValue;
 
-        const overExpressed = survivalRates.filter((rate) => rate.overexpression === 1)
-        const underExpressed = survivalRates.filter((rate) => rate.overexpression === 0);
+        const overExpressed = survivalRates.filter(
+          (rate) => rate.overexpression === 1
+        );
+        const underExpressed = survivalRates.filter(
+          (rate) => rate.overexpression === 0
+        );
 
         return {
           overexpressed: this.getPlotData(overExpressed, 'Overexpressed'),
           underexpressed: this.getPlotData(underExpressed, 'Underexpressed'),
           pValue,
           gene: param.request.gene,
-          disease: param.request.disease
+          disease: param.request.disease,
         };
-      }
+      },
     });
 
     effect(() => {
-      this.plotData.next(this.plotDataResource.value());
+      this.refresh$();
+      this.refresh();
+    });
+
+    effect(() => {
+      const data = this.plotDataResource.value();
+      const div = this.plot()?.nativeElement;
+
+      if (!data || !div) return;
+
+      const pValue = data.pValue;
+
+      Plotly.newPlot(div, [data.overexpressed, data.underexpressed], {
+        title: `Survival Analysis for ${this.getGenePrimary(data.gene)} in ${
+          data.disease.disease_name
+        }<br>P-value: ${pValue}`,
+        xaxis: {
+          title: 'Time (days)',
+        },
+        yaxis: {
+          title: 'Survival Rate',
+        },
+      });
     });
   }
 
@@ -77,39 +125,31 @@ export class KMPlotComponent implements OnInit {
     return gene.gene_symbol || gene.ensg_number;
   }
 
-  ngOnInit(): void {
-    this.plotData.subscribe((data) => {
-      if (data === undefined) {
-        return;
-      }
-
-      const pValue = data.pValue;
-
-      Plotly.newPlot(this.plot.nativeElement, [
-        data.overexpressed,
-        data.underexpressed
-      ], {
-        title: `Survival Analysis for ${this.getGenePrimary(data.gene)} in ${data.disease.disease_name}<br>P-value: ${pValue}`,
-        xaxis: {
-          title: 'Time (days)'
-        },
-        yaxis: {
-          title: 'Survival Rate'
-        }
-      });
-    });
-  }
-
   getPlotData(rates: SurvivalRate[], name: string) {
-    const sortedRates = rates.sort((a, b) => a.patient_information.survival_time - b.patient_information.survival_time);
-    const times = sortedRates.map((rate) => rate.patient_information.survival_time);
-    const events = sortedRates.map((rate) => rate.patient_information.disease_status === 0);
+    const sortedRates = rates.sort(
+      (a, b) =>
+        a.patient_information.survival_time -
+        b.patient_information.survival_time
+    );
+    const times = sortedRates.map(
+      (rate) => rate.patient_information.survival_time
+    );
+    const events = sortedRates.map(
+      (rate) => rate.patient_information.disease_status === 0
+    );
     const result = compute(times, events);
     return {
       x: result.map((point) => point.time),
       y: result.map((point) => point.rate),
       type: 'scatter',
-      name
+      name,
+    };
+  }
+
+  refresh() {
+    const plot = this.plot()?.nativeElement;
+    if (plot && plot.checkVisibility()) {
+      Plotly.Plots.resize(plot);
     }
   }
 }
