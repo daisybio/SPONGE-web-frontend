cd79e1b306164b1f81536c25acfa2bccce60fe8f
strasserle <strasser.lena@gmx.de>
2025-03-16 13:37:14 +0100

    fix disappearing pagination in prediction table

--- src/app/routes/spongeffects/predict/prediction-results/prediction-table/prediction-table.component.ts
+++ src/app/routes/spongeffects/predict/prediction-results/prediction-table/prediction-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, inject, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, computed, effect, inject, signal, ViewChild } from '@angular/core';
 import { PredictService } from '../../service/predict.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
@@ -21,13 +21,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   templateUrl: './prediction-table.component.html',
   styleUrl: './prediction-table.component.scss'
 })
-export class PredictionTableComponent {
+export class PredictionTableComponent implements AfterViewInit {
   predictService = inject(PredictService);
   prediction$ = this.predictService.prediction$;
   predictionResource = this.predictService._prediction$;
 
-  dataSource = computed(() => new MatTableDataSource(this.prediction$()?.data || []));
-
+  dataSource = new MatTableDataSource<any>([]);
+  
+  // this.prediction$()?.data || [])
   columnNames: { [key: string]: string } = {
     sampleID: 'Sample ID',
     typePrediction: 'Cancer type prediction',
@@ -39,31 +40,38 @@ export class PredictionTableComponent {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  paginator_effect = effect(() => {
-    if (this.dataSource().data.length > 0 ) {
-      this.dataSource().paginator = this.paginator;
-      this.dataSource().sort = this.sort;
-    }
-  });
+  constructor(){
+    effect(() => {
+      const data = this.prediction$()?.data || [];
+      this.dataSource.data = data;
 
-  subtype_effect = effect(() => {
-    console.log('subtype effect')
-    this.predictService._subtypes$;
-    if (this.predictService._subtypes$()) {
-      this.displayedColumns = ['sampleID', 'typePrediction', 'subtypePrediction'];
-    } else {
-      this.displayedColumns = ['sampleID', 'typePrediction'];
-    }
-    console.log(this.displayedColumns)
-    console.log(this.dataSource())
-  });
+      if (this.dataSource.data.length > 0 ) {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+    });
+
+    effect(() => {
+      this.predictService._subtypes$;
+      if (this.predictService._subtypes$()) {
+        this.displayedColumns = ['sampleID', 'typePrediction', 'subtypePrediction'];
+      } else {
+        this.displayedColumns = ['sampleID', 'typePrediction'];
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource().filter = filterValue.trim().toLowerCase();
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource().paginator) {
-      this.dataSource().paginator!.firstPage();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator!.firstPage();
     }
   }
 
